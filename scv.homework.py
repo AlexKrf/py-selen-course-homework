e86d341235caedcb45e8f32ed1073fb50d70e988
AlexKorf <alexander.posadsky@gmail.com>
2025-09-11 20:06:19 +0300

    Create scv.homework.py

---
+++ scv.homework.py
@@ -0,0 +1,45 @@
+import csv
+
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+data =[
+    ["username","password"],
+    ["user1","password1"],
+    ["user2","password2"],
+    ["user3","password3"]
+]
+with open("users.scv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+    print("Data written to users.scv")
+
+driver = webdriver.Chrome()
+driver.get("file:///C:/Users/alexander/Downloads/login.html")
+driver.maximize_window()
+
+with open("users.scv", "r", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        username, password = row
+        driver.find_element(By.XPATH, "/html/body/div[1]/p").click()
+        driver.find_element(By.ID, "register-username").send_keys(username)
+        driver.find_element(By.ID, "register-password").send_keys(password)
+        driver.find_element(By.XPATH, "/html/body/div[2]/button").click()
+        alert = driver.switch_to.alert
+        alert.accept()
+    for row in reader:
+        username, password = row
+        driver.find_element(By.XPATH, "/html/body/div[2]/p").click()
+        driver.find_element(By.ID, "login-username").send_keys(username)
+        driver.find_element(By.ID, "login-password").send_keys(password)
+        driver.find_element(By.XPATH, "/html/body/div[1]/button").click()
+        alert = driver.switch_to.alert
+        alert.accept()
+
+        time.sleep (5)
